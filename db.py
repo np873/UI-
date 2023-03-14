98d3c58ea1b3bf64a02efd4cdf0373b2b032e5d3
Niki Patel <114442831+np873@users.noreply.github.com>
2023-03-14 01:20:20 -0400

    Add files via upload

--- db.py
+++ db.py
@@ -13,12 +13,27 @@ class Database:
     # query to return all columns for n bikes in database
     def get_categories(self, n):
         data = self.select(
-            'SELECT * FROM categories ORDER BY id ASC LIMIT ?', [n])
+            'SELECT * FROM categories ORDER BY categoryid ASC LIMIT ?', [n])
         return [{
-            'id': d[0]
+            'categoryid': d[0]
             ,'image': d[1]
             ,'name': d[2]
         } for d in data]
+
+    def get_products(self, categoryid, n):
+        data = self.select(
+            'SELECT * FROM products WHERE categoryid LIKE ? ORDER BY name LIMIT ?', [categoryid, n])
+        return [{
+            'categoryid': d[0]
+            ,'productid': d[1]
+            ,'image': d[2]
+            ,'name': d[3]
+            ,'price': d[4]
+            ,'weight': d[5]
+            ,'discount': d[6]
+        } for d in data]
+
+ 
 
     def close(self):
         self.conn.close()
--- run.py
+++ run.py
@@ -3,13 +3,31 @@
 
 import os
 
-from flask import Flask, g, json, render_template, request
+from flask import Flask, g, jsonify, render_template, request
+
+from db import Database
+
+DATABASE_PATH = 'categories.db'
 
 app = Flask(__name__)
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = Database(DATABASE_PATH)
+    return db
+
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    data = get_db().get_categories(12)
+    return render_template('home.html', data = data)
 
 @app.route('/sign_in')
 def sign_in():
@@ -18,6 +36,35 @@ def sign_in():
 @app.route('/create_account')
 def create_account():
     return render_template('create_account.html')
+
+@app.route('/categories')
+def categories():
+    data = get_db().get_categories(12)
+    return render_template('categories.html', data = data) 
+
+@app.route('/api/get_categories')
+def api_get_categories():
+    n = request.args.get('n', default=12)
+    categories = get_db().get_categories(n)
+    return jsonify(categories) 
+
+@app.route('/products')
+def products():
+    categoryid = request.args.get('categoryid')
+    print(categoryid)
+    db = Database('products.db')
+    data = db.get_products(categoryid, 12)
+    return render_template('products.html', data=data)
+
+
+@app.route('/api/get_products')
+def api_get_products():
+    db = Database('products.db')
+    n = request.args.get('n')
+    categoryid = request.args.get('categoryid')
+    products = db.get_products(categoryid, n)
+    return jsonify(products)
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
