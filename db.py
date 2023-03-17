cada1c9c44ba96e7a93c5a9297e057ca57ef2650
Niki Patel <114442831+np873@users.noreply.github.com>
2023-03-16 22:22:45 -0400

    Add files via upload

--- db.py
+++ db.py
@@ -10,6 +10,11 @@ class Database:
         c.execute(sql, parameters)
         return c.fetchall()
 
+    def execute(self, sql, parameters=[]):
+        c = self.conn.cursor()
+        c.execute(sql, parameters)
+        self.conn.commit()
+
     def insert(self, sql, parameters=[]):
         c = self.conn.cursor()
         c.execute(sql, parameters)
@@ -27,7 +32,7 @@ class Database:
 
     def get_products(self, categoryid, n):
         data = self.select(
-            'SELECT * FROM products WHERE categoryid LIKE ? ORDER BY name LIMIT ?', [categoryid, n])
+            'SELECT * FROM products WHERE categoryid LIKE ? ORDER BY RANDOM() LIMIT ?', [categoryid, n])
         return [{
             'categoryid': d[0]
             ,'productid': d[1]
@@ -36,6 +41,7 @@ class Database:
             ,'price': d[4]
             ,'weight': d[5]
             ,'discount': d[6]
+            ,'discount_price': d[7]
         } for d in data]
 
     def get_product_detail(self, productid):
@@ -51,19 +57,63 @@ class Database:
             ,'price': d[4]
             ,'weight': d[5]
             ,'discount': d[6]
+            ,'discount_price': d[7]
         } for d in data]
 
-    def get_cart_items(self, productid, quantity):
+    def get_cart_items(self, cartid, quantity):
         data = self.select(
-            'SELECT * FROM cart WHERE productid = ?', [productid])
+            'SELECT * FROM cart WHERE cartid = ?', [cartid])
         return [{
             'cartid': d[0],
             'productid': d[1],
-            'name': d[2],
-            'price': d[3],
-            'weight': d[4],
-            'quantity': d[5]
+            'userid': d[2],
+            'name': d[3],
+            'price': d[4],
+            'weight': d[5],
+            'quantity': d[6],
+            'image': d[7]
         } for d in data]
+
+    def get_top_deals(self):
+        data = self.select(
+            'SELECT * FROM products WHERE discount > 10 ORDER BY RANDOM()')
+        return [{
+            'categoryid': d[0]
+            ,'productid': d[1]
+            ,'image': d[2]
+            ,'name': d[3]
+            ,'price': d[4]
+            ,'weight': d[5]
+            ,'discount': d[6]
+            ,'discount_price': d[7]
+            } for d in data] 
+
+    def remove_from_cart(self, productid):
+        print(f"Removing product {productid} from cart...")
+        rows_deleted = self.conn.execute(
+            "DELETE FROM cart WHERE productid = ?", (productid,)
+        ).rowcount
+        print(f"{rows_deleted} rows deleted.")
+
+    def create_account(self, firstname, lastname, email, password):
+        self.execute('INSERT INTO users (firstname, lastname, email, password) VALUES (?, ?, ?, ?)',
+                     [firstname, lastname, email, password])
+
+
+    def get_user(self, email):
+        data = self.select(
+            'SELECT * FROM users WHERE email=?', [email])
+        if data:
+            d = data[0]
+            return {
+                'userid': d[0],
+                'firstname': d[1],
+                'lastname': d[2],
+                'email': d[3],
+                'password': d[4],
+            }
+        else:
+            return None
 
     def close(self):
         self.conn.close()
--- run.py
+++ run.py
@@ -2,20 +2,17 @@
 # CS530: DUI, Assignment [1]
 
 import os
+from flask import redirect
 
-from flask import Flask, g, jsonify, render_template, request
+from flask import Flask, g, jsonify, render_template, request,session
 
 from db import Database
+from passlib.hash import pbkdf2_sha256
 
-import firebase_admin
-from firebase_admin import credentials, auth
-
-cred = credentials.Certificate('/Users/nikipatel/Desktop/Niki/Winter22-23/UI/Project/serviceAccountKey/serviceKey.json')
-firebase_admin.initialize_app(cred)
-
-DATABASE_PATH = 'categories.db'
+DATABASE_PATH = 'mariocart.db'
 
 app = Flask(__name__)
+app.secret_key = b'demokeynotreal!'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -32,16 +29,48 @@ def close_db(exception):
 
 @app.route('/')
 def home():
+    product = get_db().get_top_deals()
     data = get_db().get_categories(12)
-    return render_template('home.html', data=data)
+    return render_template('home.html', data=data, product=product)
 
-@app.route('/sign_in')
+@app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
-    return render_template('sign_in.html')
+    message = None
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if email and password:
+            user = get_db().get_user(email)
+            if user:
+                    return redirect('/')
+                    session['user'] = user
+                    session['logged_in'] = True
+                
+            else:
+                message = "User unknown, please try again"
+        elif email and not password:
+            message = "Missing password, please try again"
+        elif not email and password:
+            message = "Missing username, please try again"
+    return render_template('sign_in.html', message=message)
 
-@app.route('/create_account')
+@app.route('/create_account', methods=['GET', 'POST'])
 def create_account():
+    if request.method == 'POST':
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if firstname and lastname and email and password:
+            encrypted_password = pbkdf2_sha256.hash(password)
+            get_db().create_account(firstname, lastname, email, password)
+            return redirect('/sign_in')
     return render_template('create_account.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
 @app.route('/categories')
 def categories():
@@ -57,36 +86,32 @@ def api_get_categories():
 @app.route('/products')
 def products():
     categoryid = request.args.get('categoryid')
-    db = Database('products.db')
-    data = db.get_products(categoryid, 12)
+    data = get_db().get_products(categoryid, 12)
     category = get_db().get_categories(12)
-    return render_template('products.html', data=data, category=category)
-
+    return render_template('products.html', data=data, category=category) 
 
 @app.route('/api/get_products')
 def api_get_products():
-    db = Database('products.db')
     n = request.args.get('n')
     categoryid = request.args.get('categoryid')
-    products = db.get_products(categoryid, n)
+    products = get_db().get_products(categoryid, n)
     return jsonify(products)
 
 @app.route('/api/get_product_detail')
 def api_get_product_detail():
-    db = Database('products.db')
     productid = request.args.get('productid')
-    product = db.get_product_detail(productid)
+    product = get_db().get_product_detail(productid)
     return jsonify(product)
 
 
 @app.route('/product_detail')
 def product_detail():
-    return render_template('product_detail.html')
+    return render_template('product_detail.html', session=session)
 
 @app.route('/api/add_to_cart', methods=['POST'])
 def add_to_cart():
     data = request.get_json()
-    cartid = data.get('cartid')
+    cartid = session.get('cartid')
     productid = data.get('productid')
     name = data.get('name')
     price = data.get('price')
@@ -95,34 +120,47 @@ def add_to_cart():
     image = data.get('image')
 
     # Create a new CartItem object
-    db = Database('cart.db')
     productid = data.get('productid')
     quantity = data.get('quantity')
-    existing_item = db.get_cart_items(productid, quantity)
+    existing_item = get_db().get_cart_items(productid, quantity)
     if len(existing_item) == 0: 
-        data = db.insert('INSERT INTO cart (cartid, productid, name, price, quantity, weight, image) VALUES (?, ?, ?, ?, ?, ?, ?)', (cartid, productid, name, price, quantity, weight, image))
+        data = get_db().insert('INSERT INTO cart (cartid, productid, name, price, quantity, weight, image) VALUES (?, ?, ?, ?, ?, ?, ?)', (cartid, productid, name, price, quantity, weight, image))
         print(data)
     else:
-        db.insert('UPDATE cart SET quantity = quantity + ? WHERE productid = ?', (quantity, productid))
+        get_db.insert('UPDATE cart SET quantity = quantity + ? WHERE uid = ? WHERE productid = ?', (quantity, productid))
 
     return jsonify({'message': 'Product added to cart successfully.'})
 
 
 @app.route('/api/get_cart_items')
 def get_cart_items():
-    db = Database('cart.db')
-    productid = request.args.get('productid')
+    cartid = request.args.get('cartid')
     quantity = request.args.get('quantity')
-    data = db.select('SELECT * FROM cart')
-    cart = db.get_cart_items(productid, quantity)
+    data = get_db().select('SELECT * FROM cart')
+    cart = get_db().get_cart_items(productid, quantity)
     return jsonify(data)
 
 @app.route('/cart')
 def cart():
-    db = Database('cart.db')
-    data = db.select('SELECT * FROM cart')
+    data = get_db().select('SELECT * FROM cart')
     print(data)
     return render_template('cart.html', data=data)
+
+@app.route('/api/remove_from_cart', methods=['POST'])
+def remove_from_cart():
+    data = request.get_json()
+    productid = data.get('productId')
+
+    # Remove item from the cart
+    get_db().remove_from_cart(productid)
+
+    # Verify if the product is still present in the cart
+    data = get_db().select('SELECT * FROM cart WHERE productid = ?', (productid,))
+    if not data:
+        return jsonify({'message': 'Product removed from cart successfully.'})
+    else:
+        return jsonify({'message': 'Product not removed from cart.'}), 400
+
 
 
 if __name__ == '__main__':
